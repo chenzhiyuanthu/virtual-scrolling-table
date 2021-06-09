5294cefdc0aa1f72edcc5fbd9f8936179d017528
chenzhiyuanthu <chenzhiyuan-wsl@DESKTOP-M1Q669L.localdomain>
2021-06-10 03:46:23 +0800

    add

--- app.js
+++ app.js
@@ -1,50 +1,83 @@
+class Formatter {
+  static format(n, precision = 3) {
+    return Math.round(n * Math.pow(10, precision)) / Math.pow(10, precision);
+  }
+}
+
 class VirtualScrollingTable {
+  #DEFAULT_THEME = {
+    backgroundColor1: '#323232',
+    backgroundColor2: '#474747',
+    borderColor1: '#636363',
+    borderColor2: '#535353',
+    fontColor: '#ececec',
+    fontSize: '14px'
+  };
+
   constructor(config) {
     this.containerId = config.containerId;
     this.headerHeight = config.headerHeight;
     this.height = (config && config.h + 'px') || '100%';
-    this.items = config.items;
+    this.data = config.items;
+    this.updateData(this.data);
     this.itemHeight = config.itemHeight;
     this.columns = config.columns;
     this.generatorFn = (rowNumber) => {
-      const item = this.items[rowNumber];
+      const item = this.data[rowNumber];
       const row = document.createElement('div');
-      row.className = 'table-row';
-      row.style.borderBottom = '1px dotted';
-      row.style.lineHeight = config.itemHeight + 'px';
-      row.style.height = config.itemHeight + 'px';
       row.style.width = '100%';
-      row.style.display = 'flex';
-      this.columns.forEach(c => {
-        const bodyCell = document.createElement('div');
-        bodyCell.style.paddingRight = '12px';
-        bodyCell.style.width = 1 / this.columns.length * 100 + '%';
-        if (c.style) {
-          for (let i of Object.keys(c.style)) {
-            bodyCell.style[i] = c.style[i];
+      row.className = 'table-row';
+      const _tmpLineHeight = Math.ceil(config.itemHeight / 2);
+      row.style.height = config.itemHeight + 'px';
+      [0, 1].forEach(i => {
+        const _row = document.createElement('div');
+        _row.className = 'real-table-row';
+        _row.style.lineHeight = (_tmpLineHeight + 1) + 'px';
+        _row.style.height = _tmpLineHeight + 'px';
+        _row.style.width = '100%';
+        _row.style.display = 'flex';
+
+        _row.style.backgroundColor = i == 0 ?
+            this.#DEFAULT_THEME.backgroundColor1 :
+            this.#DEFAULT_THEME.backgroundColor2;
+
+        this.columns.forEach((c, index, arr) => {
+          const bodyCell = document.createElement('div');
+          bodyCell.style.paddingRight = '12px';
+          bodyCell.style.width = 1 / this.columns.length * 100 + '%';
+          index != arr.length - 1 &&
+              (bodyCell.style.borderRight =
+                   `1px solid ${this.#DEFAULT_THEME.borderColor2}`);
+          if (c.style) {
+            for (let i of Object.keys(c.style)) {
+              bodyCell.style[i] = c.style[i];
+            }
           }
-        }
-        const cellContent = document.createElement('span');
-        cellContent.innerHTML = item[c.key];
-        bodyCell.appendChild(cellContent);
-        row.appendChild(bodyCell);
+          const cellContent = document.createElement('span');
+          cellContent.innerHTML = i == 0 ? item[c.key] : '';
+          bodyCell.appendChild(cellContent);
+          _row.appendChild(bodyCell);
+        });
+        row.append(_row);
       });
       return row;
     };
 
     this.totalRows = config.totalRows || (config.items && config.items.length);
     const totalHeight = this.itemHeight * this.totalRows;
-    this.scroller = this.createScroller(totalHeight);
+    this.scroller = this._createScroller(totalHeight);
     this.container = document.getElementById(this.containerId);
-    this.header = this.createHeader();
-    this.tableBody = this.createBody();
+    this.container.style.backgroundColor = '#323232';
+    this.container.style.color = this.#DEFAULT_THEME.fontColor;
+    this.header = this._createHeader();
+    this.tableBody = this._createBody();
     this.container.appendChild(this.header);
     this.container.appendChild(this.tableBody);
 
     const screenItemsLen =
         Math.ceil((config.h - this.headerHeight) / this.itemHeight);
     const cachedItemsLen = screenItemsLen * 3;
-    this.renderChunk(this.tableBody, 0, cachedItemsLen / 2);
+    this._renderChunk(this.tableBody, 0, cachedItemsLen / 2);
 
 
     let self = this;
@@ -56,7 +89,7 @@ class VirtualScrollingTable {
       let first = parseInt(scrollTop / self.itemHeight) - screenItemsLen;
       first = first < 0 ? 0 : first;
       if (!lastRepaintY || Math.abs(scrollTop - lastRepaintY) > maxBuffer) {
-        self.renderChunk(self.tableBody, first, cachedItemsLen);
+        self._renderChunk(self.tableBody, first, cachedItemsLen);
         lastRepaintY = scrollTop;
       }
 
@@ -66,7 +99,7 @@ class VirtualScrollingTable {
     this.tableBody.addEventListener('scroll', onScroll);
   }
 
-  createBody() {
+  _createBody() {
     const tableBody = document.createElement('div');
     tableBody.className = 'vst-body';
     tableBody.style.height = `calc(100% - ${this.headerHeight}px)`;
@@ -76,29 +109,30 @@ class VirtualScrollingTable {
     return tableBody;
   }
 
-  createHeader() {
+  _createHeader() {
     const header = document.createElement('div');
     header.className = 'vst-header';
     header.style.height = this.headerHeight + 'px';
     header.style.lineHeight = this.headerHeight + 'px';
-    header.style.fontWeight = 'bold';
-    ;
-    header.style.borderBottom = '1px solid black';
+    header.style.borderTop = '2px solid #636363';
     header.style.display = 'flex';
-    this.enrichColumn();
-    this.columns.forEach(_c => {
+    this._enrichColumn();
+    this.columns.forEach((_c, index, arr) => {
       const headerCell = document.createElement('div');
       const headerName = document.createElement('span');
       headerName.innerHTML = _c.name;
       headerCell.appendChild(headerName);
       headerCell.className = 'header-cell';
+      (index != arr.length - 1) &&
+          (headerCell.style.borderRight =
+               `1px solid ${this.#DEFAULT_THEME.borderColor2}`);
       headerCell.style.width = 1 / this.columns.length * 100 + '%';
       header.appendChild(headerCell);
     });
     return header;
   }
 
-  createScroller(height) {
+  _createScroller(height) {
     const scroller = document.createElement('div');
     scroller.style.height = height + 'px';
     scroller.style.left = 0;
@@ -109,7 +143,7 @@ class VirtualScrollingTable {
     return scroller;
   }
 
-  enrichColumn() {
+  _enrichColumn() {
     this.columns.forEach(c => {
       if (c['key'] == null) {
         c['key'] = c['name'];
@@ -118,7 +152,7 @@ class VirtualScrollingTable {
     console.log(this.columns);
   }
 
-  renderChunk(node, fromPos, howMany) {
+  _renderChunk(node, fromPos, howMany) {
     let fragment = document.createDocumentFragment();
     fragment.appendChild(this.scroller);
 
@@ -138,26 +172,38 @@ class VirtualScrollingTable {
     node.innerHTML = '';
     node.appendChild(fragment);
   }
+
+  updateData(data) {
+    this.data = data;
+    this.#calculateWidth();
+  }
+
+  #calculateWidth() {
+    console.log(this.data);
+  }
 }
 
 const items = [];
-const totalRows = 10000;
+const totalRows = 10;
 
 for (let i = 0; i < totalRows; i++) {
-  items.push({a: Math.random(), b: Math.random(), c: Math.random()});
+  const q = Formatter.format;
+  items.push(
+      {a: q(Math.random()), b: q(Math.random(), 10), c: q(Math.random(), 5)});
 }
 
 const list = new VirtualScrollingTable({
   w: 600,
   h: 600,
-  itemHeight: 30,
+  itemHeight: 35,
   items: items,
   totalRows: totalRows,
   headerHeight: 50,
   containerId: 'table-container',
   columns: [
-    {name: 'A', key: 'a', style: {textAlign: 'right', fontWeight: 'normal'}},
-    {name: 'B', key: 'b', style: {textAlign: 'right'}},
-    {name: 'C', key: 'c', style: {textAlign: 'right'}}
+    {name: 'Type', key: 'a', style: {textAlign: 'right', fontWeight: 'normal'}},
+    {name: 'Car No.', key: 'b', style: {textAlign: 'right'}},
+    {name: 'Destination', key: 'c', style: {textAlign: 'right'}}
   ]
 });
+
